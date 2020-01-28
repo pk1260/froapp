f6f10807e84d47d1b57f2e3f665fb850e6a0a6a7
pk1260 <18251@ma-web.nl>
2020-01-28 15:14:04 +0100

    Gundam array commit

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,34 @@
 <template>
     <div id="app">
-
+      {{gundam}}
     </div>
 </template>
 
 <script>
     export default {
         name: 'app',
-        components: {}
-    }
+        components: {
+
+        },
+      data() {
+          return {
+            gundam: [{
+              id: 1,
+              title: "Msn-06 Sinanju",
+              price: "60$",
+            },{
+              id: 2,
+              title: "Msn-05 Sinanju",
+              price: "30$",
+            },{
+              id: 3,
+              title: "Msn-04 Sinanju",
+              price: "18$",
+            }]
+
+        }
+          }
+      }
 </script>
 
 <style>
